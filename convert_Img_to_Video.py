3f9734e2df160d51f9cbc920607e4e723b48f724
Tianle Zhu <ztl20000828@gmail.com>
2024-04-19 20:49:57 -0400

    Not perfect model

--- convert_Img_to_Video.py
+++ convert_Img_to_Video.py
@@ -2,7 +2,8 @@ import cv2
 import os
 import glob
 
-files = glob.glob('testing/camera1/*.jpg')
+files = glob.glob('generated_imgs/sort_output_images/*.jpg')
+# files = glob.glob('generated_imgs/yolo_output_images/*.jpg')
 
 files.sort(key=os.path.getmtime)
 
@@ -10,7 +11,8 @@ img = cv2.imread(files[0])
 height, width, _ = img.shape
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output_images/output.mp4', fourcc, 20.0, (width, height))
+out = cv2.VideoWriter('videos/sort_output.mp4', fourcc, 10.0, (width, height))
+# out = cv2.VideoWriter('videos/yolo_output.mp4', fourcc, 10.0, (width, height))
 
 for file in files:
     img = cv2.imread(file)
