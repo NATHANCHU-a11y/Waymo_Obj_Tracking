5a50be27599da7cac634e747658fa1e4624b07c4
Tianle Zhu <ztl20000828@gmail.com>
2024-04-19 22:09:29 -0400

    Not perfect model

--- convert_Img_to_Video.py
+++ convert_Img_to_Video.py
@@ -11,7 +11,7 @@ img = cv2.imread(files[0])
 height, width, _ = img.shape
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('videos/sort_output.mp4', fourcc, 20.0, (width, height))
+out = cv2.VideoWriter('videos/sort_output.mp4', fourcc, 10.0, (width, height))
 # out = cv2.VideoWriter('videos/yolo_output.mp4', fourcc, 10.0, (width, height))
 
 for file in files:
