b0c64a03acb1d8048162d43b87a5536e02849d21
Tianle Zhu <ztl20000828@gmail.com>
2024-04-19 22:04:19 -0400

    Not perfect model

--- numberofClasses.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Load JSON file
-with open('testing/annotations/instances_val2020.json') as f:
-    data = json.load(f)
-
-# Print categories
-for category in data['categories']:
-    print(category['name'])
